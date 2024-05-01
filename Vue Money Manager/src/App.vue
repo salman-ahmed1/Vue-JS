f49d9277f8eb70124616553e9ce85af05b53104e
Salman ahmed <salman.ahmed.sal4slm@gmail.com>
2024-05-02 00:11:31 +0600

    add vue moneyu manager app

---
+++ Vue Money Manager/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <Header /> 
+  <div class="container"> 
+    <Balance />
+    <IncomeExpenses /> 
+  </div>
+</template>
+
+
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpense.vue";
+ export default {
+    components:{
+        Header,
+        Balance,
+        IncomeExpenses
+    },
+};
+</script>
---
+++ Vue Money Manager/src/components/Balance.vue
@@ -0,0 +1,1 @@
+<template><h4>Your Balance</h4> <h1 id="Balance">$ 0.00</h1> </template>
